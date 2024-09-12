1a0a0de96b965fa16fd332c608ba1b3a068f6cfd
MuhammadUmer2223 <webomi123@gmail.com>
2024-09-13 01:33:36 +0500

    chapter 5 assignement done

---
+++ chapter5.js
@@ -0,0 +1,101 @@
+// var num1 = prompt("enter a value", 0);
+// var num2 = prompt("enter a value", 0);
+// var sum = +num1 + +num2;
+
+// var sub = +num1 - +num2;
+// var multiplication = +num1 * +num2;
+// var division = +num1 / +num2;
+// var mode = +num1 % +num2;
+
+
+// document.write("the sum of " + num1 +" and " + num2 +" is " + sum);
+
+// document.write("<br>the subtraction of " + num1 +" and " + num2 +" is " + sub);
+
+// document.write("<br>the multiplication of " + num1 +" and " + num2 +" is " + multiplication)
+// document.write("<br>the division of " + num1 +" and " + num2 +" is " + division);
+// document.write("<br>the modulous of " + num1 +" and " + num2 +" is " + mode);
+
+// var val = 10;
+// document.write("the value after variable declaration is "+ val);
+
+// var num = 20;
+// document.write("Initial value is " + num);
+ 
+// num++;
+// document.write(" <br> value after increament is "+num);
+// num = num+7;
+
+// document.write("<br>value after addition is "+ num);
+// num--;
+// document.write(" <br> value after decreament id " +num);
+// document.write(" <br>the reminder of "+ num%3);
+
+// var movieTicker = 600;
+// document.write("Total cost to buy 8 tickets" + movieTicker*5)
+// // ;
+// var num1 = 4;
+// document.write(num1 + "x" + 1 + " = " + num1*1);
+// document.write("<br>" + num1 + "x" + 2 + " = " + num1*2)
+// document.write("<br>" + num1 + "x" + 3 + " = " + num1*3);
+// document.write("<br>" + num1 + "x" + 4 + " = " + num1*4);
+// document.write("<br>" +num1 + "x" + 6 + " = " + num1*5);
+// document.write("<br>" +num1 + "x" + 7 + " = " + num1*6);
+// document.write("<br>" + num1 + "x" + 8 + " = " + num1*7);
+// document.write("<br>" + num1 + "x" + 8 + " = " + num1*8);
+// document.write("<br>" + num1 + "x" + 8 + " = " + num1*9);
+// document.write("<br>" + num1 + "x" + 11 + " = " + num1*10);
+
+// var celcius = 60;
+// var farn = (celcius * 8/5 ) + 32;
+// document.write( celcius +"% is " + farn + "%");
+
+// var item1 = 500;
+// var item2 = 600;
+// var quantity = 3;
+// var ship_charge = 100;
+// var Total =( item1 * quantity) + (item2 * quantity) + quantity + ship_charge;
+
+// document.write("price of item1 is : " +item1);
+// document.write("<br>quantity of item 1 :" + quantity);
+// document.write("<br>price of item2 is :" +item2);
+// document.write("<br>quantity of item 2 :" + quantity);
+// document.write("<br>shipping charges " + ship_charge);
+// document.write("<br>total cost  of your order is " + Total);
+
+ 
+// var marksObtained = 370;
+// var total = 400;
+// var percentage = marksObtained/total*100;
+// document.write("Total Marks" +total);
+// document.write("<br>ObtainMarks" +marksObtained);
+// document.write("<br>Percentage" +percentage);
+
+
+// var  usd = 10 *104.80;;
+// var saudi = 25 *28;
+// var total_ =  usd +saudi;
+// document.write("Total Currency in PKR" + total_);
+// var c_year = 2024;
+// var birth_year = 2000;
+// var age = c_year - birth_year;
+// document.write("Current year is " +age)
+// document.write("<br>your birth year is " + birth_year)
+
+// // document.write("<br>your age is " +age)
+// var radius = 20;
+// var circumference = 2*3.14*radius;
+// var area = 3.14* (radius*radius);
+// document.write("the raius of a circle is "+ radius);
+// document.write("<br >the circumference of circle is "+  circumference.toFixed(2));
+// document.write("<br >the area of circle is "+ area.toFixed(2));
+
+var snack = "kurleez";
+var current_age = 24;
+var max_age = 84;
+var est_amount = 4;
+var total = est_amount * (max_age -current_age);
+document.write("your favorite snack is" + snack);
+document.write("<br >your current age is " + current_age);
+document.write("<br >your estimated amount age is "+ est_amount);
+document.write("<br >you will nees " + total + "to last you until the ripe old age of " + max_age );
