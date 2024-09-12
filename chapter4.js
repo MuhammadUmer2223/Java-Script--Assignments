5e35b4bffe489d8afc5d1e4db386ed521d713be9
MuhammadUmer2223 <webomi123@gmail.com>
2024-09-12 23:18:35 +0500

    chapter 4 assignment done

---
+++ chapter4.js
@@ -0,0 +1,21 @@
+// var first_name , last_name , full_name;
+
+// declare 5legal and 5 illegal variables
+
+
+// var  name1; my_name; _name, $name2mid, fullName;        // legal variables//
+
+
+
+// illegal declaration of variables;
+// var 1name ;
+// var 23; 
+// var @name;
+// var var;
+// var %sign;
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain  Number, _ (underscore) , $ and  Letters.");
+
+// document.write("Variables must begin with a  Letter, _ or $ . For example $name, _name or name");
+// document.write("Variable names are case sensitive");
